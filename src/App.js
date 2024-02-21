f14aae4da5525b252253da636d2cbf65e867ab84
dante <dantetran04@gmail.com>
2024-02-21 17:57:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
 
 function App() {
+  const [number1, setnumber1] = useState('');
+  const [number2, setnumber2] = useState(''); 
+  const [result, setResult] = useState('');
+
+  const handleCalculate = (operator) => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+    let calculatedResult;
+
+    switch (operator) {
+      case '+':
+        calculatedResult = num1 + num2;
+        break;
+      case '-':
+        calculatedResult = num1 - num2;
+        break;
+      case '*':
+        calculatedResult = num1 * num2;
+        break;
+      case '/':
+        calculatedResult = num1 / num2;
+        break;
+      default:
+        return;
+    }
+
+    setResult(calculatedResult);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Calculator</h2>
+      <input type="number" value={number1} onChange={(e) => setnumber1(e.target.value)}/>
+      <input type="number"value={number2} onChange={(e) => setnumber2(e.target.value)}/>
+      <button onClick={() => handleCalculate('+')}>+</button>
+      <button onClick={() => handleCalculate('-')}>-</button>
+      <button onClick={() => handleCalculate('*')}>*</button>
+      <button onClick={() => handleCalculate('/')}>/</button>
+      <h3>Result: {result}</h3>
     </div>
   );
 }
